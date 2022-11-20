9f735b6f65731da412a98ce820c0e63695b739fc
mikilukasik <szarhazi@gmail.com>
2022-11-20 16:33:23 +0000

    updated grid profile (hardcoded)

--- src/aiClient/methods/grid.js
+++ src/aiClient/methods/grid.js
@@ -1,10 +1,12 @@
 import { engineSocket } from '../engineSocket.js';
 
 const aiMultiplier = 2;
+
 const deepMoveSorters = [
-  { modelName: 'pg_large', cutoff: 0.0001 },
-  { modelName: 'pg_small', cutoff: 0, worker: 'main' /*, cutoff: 0.042*/ },
-  { modelName: 'pg_tiny', cutoff: 0, worker: 'sub' /*, cutoff: 0.042*/ },
+  // { modelName: 'pg_large', cutoff: 0.0001 },
+  { modelName: 'pg_ML', cutoff: 0.00005 },
+  { modelName: 'pg_small', worker: 'main', cutoff: 0.0005 },
+  { modelName: 'pg_tiny', worker: 'sub', cutoff: 0.0005 },
 ];
 
 export const grid = async ({ depth, game }) => {
